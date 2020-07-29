f123ab86980111adc7e4e62d730c5ae41daf8429
temme <temme@pop-os.localdomain>
2020-07-29 21:24:13 +0530

    ReLu & Activation

--- p2.py
+++ p2.py
@@ -6,7 +6,6 @@ nnfs.init()
 
 X, y = spiral_data(100, 3)   
 
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -22,9 +21,7 @@ class Activation_ReLU:
 
 layer1 = Layer_Dense(2,5)
 activation1 = Activation_ReLU()
-
 layer1.forward(X)
 
-#print(layer1.output)
 activation1.forward(layer1.output)
 print(activation1.output)
